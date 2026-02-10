5a71ec56a5b244ccf6462c378a9dde0ee8fe1b86
scott-uni <s.anvarov@newuu.uz>
2026-02-10 11:44:26 +0500

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\"I'm standing\" on the edge of some crazy cliff.\n";
+    cout << "What I have to do, I have to catch everybody\\\n";
+    cout << "if they start to go over the cliff—I mean\\\n";
+    cout << "\\if they're running and they don't look\n";
+    cout << "where they're going I have to come out\n";
+    cout << "from somewhere and catch them.\n";
+    return 0;
+}
